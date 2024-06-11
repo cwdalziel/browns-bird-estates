4dc977dfaf8bcceed3821ce013c07abe00fd0865
carson <carsonwdalziel@gmail.com>
2024-06-10 20:45:20 -0700

    all pages done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,11 +3,15 @@ import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import styled from "styled-components";
+import { Add, Remove } from "@mui/icons-material";
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
-  padding: 20px;
+  margin: 10px;
+  padding: 10px;
+  background-color: white;
+  border-radius: 5px;
 `;
 
 const Title = styled.h1`
@@ -26,6 +30,7 @@ const TopButton = styled.button`
   padding: 10px;
   font-weight: 600;
   cursor: pointer;
+  border-radius: 5px;
   border: ${(props) => props.type === "filled" && "none"};
   background-color: ${(props) => props.type === "filled" && "#588764"};
   color: ${(props) => props.type === "filled" && "white"};
@@ -48,16 +53,101 @@ const Info = styled.div`
   flex: 3;
 `;
 
-const Product = styled.div``
-const ProductDetail = styled.div``
-const Image = styled.img``
-const Details = styled.div``
-const ProductName = styled.span``
-const ProductId = styled.span``
-const PriceDetail = styled.div``
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 5px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProductName = styled.span`
+  margin-bottom: 10px;
+`;
+
+const ProductId = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+  margin-bottom: 20px;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const Hr = styled.hr`
+  background-color: #e9e9e9;
+  border: none;
+  height: 1px;
+  padding: 0px 30px;
+`;
 
 const Summary = styled.div`
   flex: 1;
+  background-color: #e9e9e9;
+  border-radius: 10px;
+  padding: 20px;
+  margin-left: 10px;
+  height: 50vh;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+  margin: 10px 0px;
+`;
+
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => (props.type === "total" ? "700" : "500")};
+  font-size: ${(props) => (props.type === "total" ? "24px" : "20px")};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  cursor: pointer;
+  border: none;
+  background-color: #588764;
+  color: white;
+  font-size: 20px;
+  border-radius: 5px;
 `;
 
 const Cart = () => {
@@ -78,19 +168,69 @@ const Cart = () => {
         <Bottom>
           <Info>
             <Product>
-                <ProductDetail>
-                    <Image src="https://i.redd.it/gk45grax5w061.jpg" />
-                    <Details>
-                        <ProductName><b>Product:</b> GRANGSTER SPONGE</ProductName>
-                        <ProductId><b>ID:</b> GRANGSTER SPONGE</ProductId>
-                    </Details>
-                </ProductDetail>
-                <PriceDetail>
-                    Price
-                </PriceDetail>
+              <ProductDetail>
+                <Image src="https://i.redd.it/gk45grax5w061.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> GRANGSTER SPONGE
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> GRANGSTER SPONGE
+                  </ProductId>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src="https://i.redd.it/gk45grax5w061.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> GRANGSTER SPONGE
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> GRANGSTER SPONGE
+                  </ProductId>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
             </Product>
           </Info>
-          <Summary>Summary</Summary>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -5</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
         </Bottom>
       </Wrapper>
       <Footer />
