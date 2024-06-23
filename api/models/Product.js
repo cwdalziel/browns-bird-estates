485c56608a5ba86ebdf836a3c81c5d7cc993ad10
carson <carsonwdalziel@gmail.com>
2024-06-22 18:32:05 -0700

    finished routes

--- api/models/Product.js
+++ api/models/Product.js
@@ -6,7 +6,7 @@ const ProductSchema = new mongoose.Schema(
     desc: { type: String, required: true },
     img: { type: String, required: true },
     categories: { type: Array },
-    price: { type: String, required: true },
+    price: { type: Number, required: true },
   },
   { timestamps: true }
 );
