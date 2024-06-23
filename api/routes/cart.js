cd0f421bba0e26a9f851b3eebef60b678c34ba2f
carson <carsonwdalziel@gmail.com>
2024-06-23 16:58:05 -0700

    added stripe route

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -46,7 +46,7 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
-//GET USER CART
+//GET USER CART !possible bug!
 router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
   try {
     const cart = await Cart.findOne({ userId: req.params.userId });
